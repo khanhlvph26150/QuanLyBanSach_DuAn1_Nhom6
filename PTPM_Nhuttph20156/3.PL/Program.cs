5f51e96aa21cfb36749ed336577b0c34296ad7c2
khanhlvph26150 <khanhlvph26150@fpt.edu.vn>
2022-11-10 16:26:41 +0700

    deleted more random garbage

--- PTPM_Nhuttph20156/3.PL/Program.cs
+++ PTPM_Nhuttph20156/3.PL/Program.cs
@@ -19,10 +19,7 @@ namespace _3.PL
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new FrmAll());
-            //blabajfoifjawoi
-            //foajoiaejfiewjfwe
-            //aoeijfoiewjfaow
-            //foiawfjoweijewoij
+           
         }
     }
 }
